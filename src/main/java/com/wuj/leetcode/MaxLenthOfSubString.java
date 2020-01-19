edae2fff984159c101915b0ed9e18139032bb57c
xiaoyi <xiaoyi@gegejia.com>
2020-01-19 23:43:46 +0800

    不含重复字符的最大长度

--- src/main/java/com/wuj/leetcode/MaxLenthOfSubString.java
+++ src/main/java/com/wuj/leetcode/MaxLenthOfSubString.java
@@ -1,8 +1,36 @@
 package com.wuj.leetcode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author xiaoyi
  */
 public class MaxLenthOfSubString {
 
+    public static int lengthOfLongestSubstring(String s) {
+
+        if(s == null || "".equals(s)){
+            return 0;
+        }
+
+        int length = 0;
+        int start = 0;
+        Map<Character,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            Character character = s.charAt(i);
+            if(map.get(character) != null){
+                start = Math.max(map.get(character) + 1,start);
+            }
+            map.put(character,i);
+            length = Math.max(length,i -start + 1);
+        }
+        return length;
+    }
+
+    public static void main(String[] args) {
+        int length = lengthOfLongestSubstring("abba");
+        System.out.println("长度为：" + length);
+    }
 }
